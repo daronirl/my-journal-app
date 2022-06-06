9f50f144c21c80a12ac2898ca05365dd035a4241
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-06-06 15:20:44 -0700

    added styling to main css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,11 @@ function Main(props) {
   
   return (
 
-    <Container fluid className='text-center'>
+    <Container fluid className='col--whole text-center pt-4'>
       <Row>
-       <Col className='col--1'>
-       <img src={`./images/${props.item.imageUrl}`} className='l--nv'/>
+        <Col className='col--1'>
+           <img src={`./images/${props.item.imageUrl}`} className='l--nv'/>
+                <h3>{props.item.title} <span className='google--map'><a href={props.item.googleMapsUrl} target="_blank">View on Google Maps</a></span></h3>
                 {props.item.location}
                 {props.item.description}
        
