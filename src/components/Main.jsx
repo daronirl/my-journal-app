0b20bb5a5686c9a1ec6a49298745555d980b8914
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-06-07 17:44:21 -0700

    used props to pull more data from a data file. Added some more locations I visited

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,31 +6,11 @@ function Main(props) {
   
   return (
 
-    <Container fluid className='col--whole text-center pt-4'>
+    <Container fluid="lg" className='col--whole text-center pt-4'>
       <Row>
         <Col className='col--1'>
            <img src={`./images/${props.item.imageUrl}`} className='l--nv'/>
-               <div className='card--info'>
-                <h4 className='loc--name'>{props.item.location} <a href={props.item.googleMapsUrl} target="_blank">
-                  <span className='google--map'>View on Google Maps</span></a></h4>
-                     <h2>{props.item.title}</h2>
-                        <p>{props.item.description}</p>
-            </div>
-       
-       </Col>
-       <Col className='col--1'>
-           <img src={`./images/${props.item.imageUrl}`} className='l--nv'/>
-               <div className='card--info'>
-                <h4 className='loc--name'>{props.item.location} <a href={props.item.googleMapsUrl} target="_blank">
-                  <span className='google--map'>View on Google Maps</span></a></h4>
-                     <h2>{props.item.title}</h2>
-                        <p>{props.item.description}</p>
-            </div>
-       
-       </Col>
-       <Col className='col--1'>
-           <img src={`./images/${props.item.imageUrl}`} className='l--nv'/>
-               <div className='card--info'>
+               <div className='card--info pt-2'>
                 <h4 className='loc--name'>{props.item.location} <a href={props.item.googleMapsUrl} target="_blank">
                   <span className='google--map'>View on Google Maps</span></a></h4>
                      <h2>{props.item.title}</h2>
@@ -40,6 +20,9 @@ function Main(props) {
        </Col>
     </Row>
   </Container>
+  
+
+  
   )
 }
 
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -5,7 +5,25 @@ export default [
         title: "Laughlin",
         location: "Nevada",
         googleMapsUrl: "https://goo.gl/maps/tLn5PQjvYj1pRkG49",
-        description: "Laughlin is a fun place to visit on vacation. They have drinking, gambling, jet skiing, and hiking to name a few",
+        description: "Laughlin is an unincorporated resort town and census-designated place in Clark County, Nevada, United States. It is located on the Colorado River, directly across from the much larger Bullhead City, Arizona.",
+        imageUrl: "lnv.jpg"
+    },
+
+    {
+        id: 2,
+        title: "Solvang",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/B47BPPFhmsj5PF1y6",
+        description: "Solvang is a city in southern California's Santa Ynez Valley. It's known for its Danish-style architecture and many wineries. The Elverhøj Museum of History & Art explores the city's Danish heritage through personal stories and photographs. ",
+        imageUrl: "lnv.jpg"
+    },
+
+    {
+        id: 3,
+        title: "Disneyland",
+        location: "California",
+        googleMapsUrl: "https://goo.gl/maps/VQd6bWNbRNfMtQxa7",
+        description: "Disneyland is an amusement park in Anaheim, California, which opened in 1955. It is the only one designed and built under the direct supervision of Walt Disney.",
         imageUrl: "lnv.jpg"
     }
 
