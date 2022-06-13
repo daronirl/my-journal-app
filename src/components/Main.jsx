b3d0ad9c2fe8539898f8571a31c1b624306c2bcf
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-06-13 13:28:26 -0700

    Fixed media queries for all screen sizes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,16 +9,16 @@ function Main(props) {
     <Container fluid="lg" className='col--whole text-center pt-4'>
       <Row>
         <Col className='col--1'>
-           <img src={`./images/${props.item.imageUrl}`} className='pic--loc'/>
+           <img src={`./images/${props.item.imageUrl}`} alt="destinations" className='pic--loc'/>
                <div className='card--info pt-2'>
-                <h4 className='loc--name'>{props.item.location} <a href={props.item.googleMapsUrl} target="_blank">
-                  <span className='google--map'>View on Google Maps</span></a></h4>
-                     <h2>{props.item.title}</h2>
-                        <p className='desc'>{props.item.description}</p>
-            </div>
+                 <h4 className='loc--name'>{props.item.location} <a href={props.item.googleMapsUrl} rel="noreferrer" target="_blank">
+                   <span className='google--map'>View on Google Maps</span></a></h4>
+                      <h2>{props.item.title}</h2>
+                         <p className='desc'>{props.item.description}</p>
+                   </div>
        
-       </Col>
-    </Row>
+          </Col>
+        </Row>
   </Container>
   
 
